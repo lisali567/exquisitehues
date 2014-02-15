2db25f717d5cdb10922ab3a0bfaa60552df36b56
Melissa O'Sullivan <mko2110@columbia.edu>
2014-02-15 18:17:09 -0500

    adding loop for poetry lines

--- web.js
+++ web.js
@@ -3,17 +3,26 @@ var logfmt = require("logfmt");
 var app = express();
 var twilio = require("twilio");
 var Firebase = require('firebase');
+var line = 1;
+var fbase = new Firebase('https://flickering-fire-2682.firebaseio.com/');
 
 app.use(logfmt.requestLogger());
 app.use(express.bodyParser());
 
 app.post('/sms', function(req, res) {
+
+	//poem
     var twiml = new twilio.TwimlResponse();
     twiml.message('Hello World, you said: ' + req.body.Body);
-    var fbase = new Firebase('https://flickering-fire-2682.firebaseio.com/');
+    
     fbase.push({'number':req.body.From, 'text':req.body.Body});
     res.writeHead(200, {'Content-Type': 'text/xml'});
     res.end(twiml.toString());
+
+    if( line == 4){
+    	fbase = new Firebase('https://flickering-fire-2682.firebaseio.com/');
+    }
+    line++;
 });
 
 var port = Number(process.env.PORT || 5000);
