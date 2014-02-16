4a50ddc8097b099bd4a189e50729b424a4fda6a7
Melissa O'Sullivan <mko2110@columbia.edu>
2014-02-15 22:56:01 -0500

    added stuff to be able to make colors l8r

--- web.js
+++ web.js
@@ -8,8 +8,7 @@ var fbase = new Firebase('https://flickering-fire-2682.firebaseio.com/');
 var newPoem;
 var prevLine;
 var teleNum = [];
-//if firstTime is true, should NOT take body as line 1
-//var firstTime = false;
+var poemString = "";
 
 
 app.use(logfmt.requestLogger());
@@ -17,48 +16,43 @@ app.use(express.bodyParser());
 
 app.post('/sms', function(req, res) {
 
-    //poem
     var twiml = new twilio.TwimlResponse();
     var body = req.body.Body;
     var from = req.body.From;
 
-    //Sentence to join: 'Join the poem!'
     var numIndex = -1;
     for(i=0;i<teleNum.length;i++){
-    	if(from == teleNum[i]){
+    	if(from === teleNum[i]){
     		numIndex = i;
     	}
     }
 
 	if(numIndex != -1)
 	{   
-			if(line==0) {
+			if(line===0) {
 			    newPoem = fbase.push({'counter':line});
-			  //  prevLine = req.body.Body;
 			}
-		//	else {
-		//	    twiml.message('Previous Line: ' + prevLine);
-    	//		}
 			newLine = newPoem.push({'number':req.body.From, 'text':req.body.Body});
 			prevLine = req.body.Body;
+			poemString += prevLine;
 			line++;
 			teleNum = [];
-			//teleNum.push(from);
-			if(line == 4){
+			if(line === 4){
 			    line = 0;
-			//    teleNum.length=0;
+			    poemString = "";
 			}
 
 			twiml.message("Thanks for adding a line to the poem");
-			
-			//add the line
+
+			//ADD HUE stuff here send count, poemString, and prevLine to be analyzed
+
 	}
 	else{
 			if(line==0) {
 				twiml.message("Start a new poem!");
 			}
 			else{
-				twiml.message("Here's the last line: " + prevLine);
+				twiml.message("Here's the last line: " + prevLine + "/nrespond with the next one!");
 			}
 			teleNum.push(from);
 	}
