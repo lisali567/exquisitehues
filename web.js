bbc2b211bd5af9c5cda59cd0a102a4f6623b6a71
Lisa Li <ll2768@columbia.edu>
2014-02-15 20:38:08 -0500

    hmmm

--- web.js
+++ web.js
@@ -27,41 +27,50 @@ app.post('/sms', function(req, res) {
     for(i=0;i<teleNum.length;i++){
 	if(from==teleNum[i])
 	    {
+		//if not new & trying to pretend to be
 		if(body=="Join the poem!")
 		    {
 			twim1.message("lol");
 			return;
 		    }
+		else {
+		    while(!firstTime) {   
+			if(line==0) {
+			    newPoem = fbase.push({'counter':line});
+			    
+			    prevLine = req.body.Body;
+			}
+			else {
+			    twiml.message('Previous Line: ' + prevLine);
+    }
+			newLine = newPoem.push({'number':req.body.From, 'text':req.body.Body});
+			prevLine = req.body.Body;
+			line++;
+			teleNum.push(from);
+			if( line == 4){
+			    line = 0;
+			    teleNum.length=0;
+			}
+			
+			//add the line
+		    }
+		}
 	    }
-    }
-    if(body=="Join the poem!")
-	{
-	    twiml.message("Start a new poem!");
-	    firstTime = true;
+	//not in teleNum, so check if joining
+	//if joining, do nothing; go again
+	//if not joining but new say something
+	if(body=="Join the poem!")
+	    {
+		twiml.message("Start a new poem!");
+		firstTime = true;
+	    }
+	else { twiml.message("sorry bro, you gotta join first!");
 	}
-    while(!firstTime) {   
-    if(line==0) {
-	newPoem = fbase.push({'counter':line});
+	res.writeHead(200, {'Content-Type': 'text/xml'});
+	res.end(twiml.toString());
 
-	prevLine = req.body.Body;
-    }
-    else {
-    twiml.message('Previous Line: ' + prevLine);
-    }
-    newLine = newPoem.push({'number':req.body.From, 'text':req.body.Body});
-    prevLine = req.body.Body;
-    line++;
-    teleNum.push(from);
-    if( line == 4){
-	line = 0;
-	teleNum.length=0;
-    }
-    
-    res.writeHead(200, {'Content-Type': 'text/xml'});
-    res.end(twiml.toString());
-    }
-    firstTime=false;
-});
+	firstTime=false;
+    });
 
 var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
